c794a33f2a895be42934c8c22037667c482dccdf
miki <m.ippoliti@proton.me>
2024-05-15 23:10:39 +0200

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -5,6 +5,8 @@ Preload.prototype = {
 	preload: function(){ 
 		this.game.load.image('tile', 'assets/tile.png');
 		this.game.load.image('box', 'assets/box.png');
+		this.game.load.image('box', 'assets/commesse.png');
+
 		
 		this.game.load.spritesheet('player', 'assets/player.png', 24, 24, 9, -2);
 		
